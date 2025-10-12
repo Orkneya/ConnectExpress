9b7c3a5b1f6bc9103ef29da6519704f75350aee1
Tetyana Popenko <orkneya17@gmail.com>
2025-10-12 23:59:38 +0300

    servises

--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -1,3 +1,4 @@
+import style from "./ServicesPage.module.css";
 function ServicesPage() {
   return (
     // <div>
@@ -5,25 +6,25 @@ function ServicesPage() {
     //   <p>Оберіть потрібний тип перевезення з меню.</p>
     // </div>
 
-    <section class="services-page">
-      <div class="hero">
-        <h1 class="hero-title">Послуги</h1>
+    <section className={style.servicesPage}>
+      <div className={style.hero}>
+        <h1 className={style.heroTitle}>Послуги</h1>
       </div>
 
-      <div class="services-grid">
-        <a href="/service1" class="service-item">
+      <div className={style.servicesGrid}>
+        <a href="/service1" className={style.serviceItem}>
           <img src="img/service1.jpg" alt="Послуга 1" />
         </a>
-        <a href="/service2" class="service-item">
+        <a href="/service2" className={style.serviceItem}>
           <img src="img/service2.jpg" alt="Послуга 2" />
         </a>
-        <a href="/service3" class="service-item">
+        <a href="/service3" className={style.serviceItem}>
           <img src="img/service3.jpg" alt="Послуга 3" />
         </a>
-        <a href="/service4" class="service-item">
+        <a href="/service4" className={style.serviceItem}>
           <img src="img/service4.jpg" alt="Послуга 4" />
         </a>
-        <a href="/service5" class="service-item">
+        <a href="/service5" className={style.serviceItem}>
           <img src="img/service5.jpg" alt="Послуга 5" />
         </a>
       </div>

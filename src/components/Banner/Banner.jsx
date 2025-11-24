c76b0f36599947c17ec602d69747015137d5695b
Tetyana Popenko <orkneya17@gmail.com>
2025-11-24 13:16:10 +0200

    not translate title

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,9 @@ import styles from "./Banner.module.css";
 export default function Banner() {
   return (
     <section className={styles.section}>
-      <h1 className={styles.title}>Services of your dreams</h1>
+      <h1 translate="no" className={styles.title}>
+        Services of your dreams
+      </h1>
     </section>
   );
 }
